0f99d1f1b55d1ae4d50502b4842634b280d752cd
Yusuph <115868619+T3chnocr4t@users.noreply.github.com>
2024-09-07 12:55:07 +0100

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,1 +1,14 @@
+# This script was written by [T3chnocr4t]
+# This is my python program for my task 01 (prodigy info tech internship program) 
+# Task 01 -> Implement Caesar cipher 
+
+def display_author_name():
+    author_name = "[T3chnocr4t]" 
+    print("------------------------------------------------")
+    print("This script was written by:")
+    print()
+    print(f"    {author_name}")
+    print()
+    print("------------------------------------------------")
+display_author_name()
 
